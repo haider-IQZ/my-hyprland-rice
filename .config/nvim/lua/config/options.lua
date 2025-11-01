abda8f856e3d4b0c0487474afc87a1c7387ca3ac
haider-IQZ <haidersoka@tuta.io>
2025-11-02 00:35:54 +0300

    Disable inline ghost text and inlay hints in Neovim
    - Disabled ghost text suggestions (inline gray text)
    - Only show popup completion menu
    - Cleaner coding experience

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable inline ghost text suggestions
+vim.g.cmp_ghost_text = false
+
+-- Disable inlay hints (the grayed out type annotations)
+vim.g.lazyvim_rust_diagnostics = false
