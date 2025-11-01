a5439c9ff2631b758788f5e385774724b084a9e0
haider-IQZ <haidersoka@tuta.io>
2025-11-02 01:11:12 +0300

    Make Neovim follow wallpaper colors with matugen
    - Created neovim color template for matugen
    - Neovim now uses same colors as foot/waybar/hyprland
    - Colors change automatically with wallpaper
    - Transparent background maintained

---
+++ .config/matugen/templates/neovim-colors.lua
@@ -0,0 +1,25 @@
+-- Auto-generated colors from matugen
+return {
+  background = "#{{ colors.background.default.hex_stripped }}",
+  foreground = "#{{ colors.on_surface.default.hex_stripped }}",
+  
+  -- Base colors
+  color0 = "#{{ colors.surface.default.hex_stripped }}",
+  color1 = "#{{ colors.error.default.hex_stripped }}",
+  color2 = "#{{ colors.primary.default.hex_stripped }}",
+  color3 = "#{{ colors.tertiary.default.hex_stripped }}",
+  color4 = "#{{ colors.on_primary_container.default.hex_stripped }}",
+  color5 = "#{{ colors.on_secondary_container.default.hex_stripped }}",
+  color6 = "#{{ colors.secondary.default.hex_stripped }}",
+  color7 = "#{{ colors.on_surface.default.hex_stripped }}",
+  
+  -- Bright colors
+  color8 = "#{{ colors.surface_bright.default.hex_stripped }}",
+  color9 = "#{{ colors.error.default.hex_stripped }}",
+  color10 = "#{{ colors.primary.default.hex_stripped }}",
+  color11 = "#{{ colors.tertiary.default.hex_stripped }}",
+  color12 = "#{{ colors.on_primary_container.default.hex_stripped }}",
+  color13 = "#{{ colors.on_secondary_container.default.hex_stripped }}",
+  color14 = "#{{ colors.secondary.default.hex_stripped }}",
+  color15 = "#{{ colors.on_surface.default.hex_stripped }}",
+}
---
+++ .config/nvim/lua/colors/matugen.lua
@@ -0,0 +1,25 @@
+-- Auto-generated colors from matugen
+return {
+  background = "#19120c",
+  foreground = "#efe0d5",
+  
+  -- Base colors
+  color0 = "#19120c",
+  color1 = "#ffb4ab",
+  color2 = "#feb876",
+  color3 = "#c1cc99",
+  color4 = "#ffdcbf",
+  color5 = "#ffdcbf",
+  color6 = "#e2c0a4",
+  color7 = "#efe0d5",
+  
+  -- Bright colors
+  color8 = "#403730",
+  color9 = "#ffb4ab",
+  color10 = "#feb876",
+  color11 = "#c1cc99",
+  color12 = "#ffdcbf",
+  color13 = "#ffdcbf",
+  color14 = "#e2c0a4",
+  color15 = "#efe0d5",
+}
