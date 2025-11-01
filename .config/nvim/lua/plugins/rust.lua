56a4b43e961f9c9f3e80de01e9ea4611a36602a6
haider-IQZ <haidersoka@tuta.io>
2025-11-02 00:33:34 +0300

    Add Rust LSP config for Neovim
    - Configured rust-analyzer with clippy checks
    - Enabled all cargo features
    - Added proc macro support
    - Error checking on save

---
+++ .config/nvim/lua/plugins/rust.lua
@@ -0,0 +1,34 @@
+return {
+  -- Add rust-analyzer LSP
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
