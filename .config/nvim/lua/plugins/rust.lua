f613118040455c94c6213ca41acddc42699e5d62
haider-IQZ <haidersoka@tuta.io>
2025-11-02 00:39:11 +0300

    Re-enable rust-analyzer but disable ALL inlay hints
    - Error checking enabled
    - Clippy diagnostics enabled
    - ALL inlay hints disabled (no more inline type annotations)
    - Clean code view with error checking

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -1,11 +1,31 @@
 return {
-  -- Disable rust-analyzer completely
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         rust_analyzer = {
-          enabled = false,
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+              },
+              checkOnSave = {
+                command = "clippy",
+              },
+              -- DISABLE ALL INLAY HINTS
+              inlayHints = {
+                bindingModeHints = { enable = false },
+                chainingHints = { enable = false },
+                closingBraceHints = { enable = false },
+                closureReturnTypeHints = { enable = false },
+                lifetimeElisionHints = { enable = false },
+                parameterHints = { enable = false },
+                reborrowHints = { enable = false },
+                renderColons = false,
+                typeHints = { enable = false },
+              },
+            },
+          },
         },
       },
     },
